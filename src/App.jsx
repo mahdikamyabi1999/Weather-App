af55898b5d3b47b329127eb8900b5822c1ae4513
mahdi <150438109+mahdikamyabi1999@users.noreply.github.com>
2024-02-20 14:35:16 +0330

    get data from API with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Weather from "./components/Weather";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const API_KEY = '1ac5fc8b0c6716a702e32ccf86a5b5eb';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
 
+  const searchLoction = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      });
+      setLocation("");
+    }
+  };
   return (
-    <div></div>
-  )
+    <div className="w-full h-full relative">
+      <div className="text-center p-4">
+        <input
+          type="text"
+          className="py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md"
+          placeholder="Enter location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyDownCapture={searchLoction}
+        />
+      </div>
+      <Weather weatherData={data}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
